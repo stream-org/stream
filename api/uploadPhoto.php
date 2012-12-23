af59f751f52e82ad1788e4954ccf68e590424477
Rousseau Kazi <kazi.rousseau@gmail.com>
2012-12-23 00:31:53 -0800

    uploadPhoto update

--- api/uploadPhoto.php
+++ api/uploadPhoto.php
@@ -9,7 +9,8 @@
 //output::
 //	pictureID or null
 
-include "connection.php";
+include('SimpleImage.php');
+include('connection.php');
 
 //grabbing the arguments 
 $phone = $_GET['phone'];
@@ -19,24 +20,81 @@ $streamID = $_GET['streamID'];
 $pictureID = $picture . $phone . $streamID . time();
 $pictureID = hash('sha512', $pictureID);
 
-mysql_query("INSERT INTO StreamActivity (StreamID, Phone, PictureID, PicURL, TinyPicURL) VALUES ('$streamID', '$phone','$picture', '$pictureID', '$tiny')");
+// Logic for photos uploaded through text
 
-$result = mysql_query("SELECT * FROM StreamActivity WHERE PictureID='$picture' AND Phone='$phone' AND StreamID='$streamID'");
-$responseArray = array();
+if ($tiny = "null"){
 
-$row = mysql_fetch_array($result);
 
-if (empty($row)) 
-{
-	$responseArray = null;
-} 
-else 
-{
-	$responseArray['pictureID'] = $pictureID;
+	echo "Mogreet!";
+	echo "<br>Picture: ".$picture;
+
+	$val = time().$picture;
+	$filename = hash('sha512', $val) . '.jpg';
+
+	echo "<br> filename: ".$filename;
+
+	exec("touch ".$filename);
+	exec("chmod go+w ".$filename);
+	$command = "wget -O ".$filename." ".$picture;
+	exec($command);
+	exec("mv ".$filename." ~/html/upload/StreamPictures/Pictures/");
+
+	$filePath = '~/html/upload/StreamPictures/Pictures/' . $filename;
+
+	$pictureFilePath = 'http://75.101.134.112/upload/StreamPictures/Pictures/' . $filename;
+	$tinyPictureFilePath = 'http://75.101.134.112/upload/StreamPictures/TinyPictures/tiny' . $filename;
+	$tinyfilename = "tiny".$filename;
+
+	list($width, $height) = getimagesize($filePath);
+
+	if(intval($width) >= intval($height))
+	{
+		
+		chdir('../');
+		chdir('upload');
+		chdir('StreamPictures');
+		chdir('Pictures');		
+		$image = new SimpleImage();
+		$image->load($filename);
+		chdir('../');	
+		chdir('TinyPictures');
+		$image->resizeToWidth(600);
+		$image->save($tinyfilename);
+	}
+	else
+	{
+		chdir('../');
+		chdir('upload');
+		chdir('StreamPictures');
+		chdir('Pictures');
+		$image = new SimpleImage();
+		$image->load($filename);
+		chdir('../');
+		chdir('TinyPictures');
+		$image->resizeToHeight(600);
+		$image->save(tinyfilename);
+	}
+
+	mysql_query("INSERT INTO StreamActivity (StreamID, Phone, PictureID, PicURL, TinyPicURL) VALUES ('$streamID', '$phone','$pictureFilePath', '$pictureID', '$tinyPictureFilePath')");
+
+ }
+
+else{
+	mysql_query("INSERT INTO StreamActivity (StreamID, Phone, PictureID, PicURL, TinyPicURL) VALUES ('$streamID', '$phone','$picture', '$pictureID', '$tiny')");
+
+	$result = mysql_query("SELECT * FROM StreamActivity WHERE PictureID='$picture' AND Phone='$phone' AND StreamID='$streamID'");
+	$responseArray = array();
+
+	$row = mysql_fetch_array($result);
+
+	if (empty($row)) 
+	{
+		$responseArray = null;
+	} 
+	else 
+	{
+		$responseArray['pictureID'] = $pictureID;
+	}
+
+	echo json_encode($responseArray);
 }
-
-echo json_encode($responseArray);
-
-
-
-?>
