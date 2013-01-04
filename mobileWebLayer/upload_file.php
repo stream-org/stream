1c0becb8f29488610fda80ee1ba9498d9768db56
sumanv <svenkat45@gmail.com>
2013-01-03 19:22:22 -0800

    upload_file fixed to not allow creation of null photos. Created script to delete empty photos

--- mobileWebLayer/upload_file.php
+++ mobileWebLayer/upload_file.php
@@ -6,45 +6,59 @@ include('connection.php');
 $phone = $_POST['phoneNumber'];
 $streamID = $_POST['streamID'];
 
-$_FILES["file"]["name"] = hash('sha512', time()) . '.jpg';
-$filename = $_FILES["file"]["name"];
-
-move_uploaded_file($_FILES["file"]["tmp_name"], 'StreamPictures/Pictures/' . $filename);
-
-$filePath = 'StreamPictures/Pictures/' . $filename;
-
-$pictureFilePath = 'http://75.101.134.112/upload/StreamPictures/Pictures/' . $filename;
-$tinyPictureFilePath = 'http://75.101.134.112/upload/StreamPictures/TinyPictures/' . $filename;
+if(($_FILES["file"]["size"] > 10) && ($_FILES["file"]["size"] < 20000000)){
 
 
-list($width, $height) = getimagesize($filePath);
 
-if(intval($width) >= intval($height))
-{
-	chdir('StreamPictures');
-	chdir('Pictures');
-	$image = new SimpleImage();
-	$image->load($filename);
-	chdir('../');
-	chdir('TinyPictures');
-	$image->resizeToWidth(600);
-	$image->save($filename);
+
+	$_FILES["file"]["name"] = hash('sha512', time()) . '.jpg';
+	$filename = $_FILES["file"]["name"];
+
+	move_uploaded_file($_FILES["file"]["tmp_name"], 'StreamPictures/Pictures/' . $filename);
+
+	$filePath = 'StreamPictures/Pictures/' . $filename;
+
+	$pictureFilePath = 'http://75.101.134.112/upload/StreamPictures/Pictures/' . $filename;
+	$tinyPictureFilePath = 'http://75.101.134.112/upload/StreamPictures/TinyPictures/' . $filename;
+
+
+	list($width, $height) = getimagesize($filePath);
+
+	if(intval($width) >= intval($height))
+	{
+		chdir('StreamPictures');
+		chdir('Pictures');
+		$image = new SimpleImage();
+		$image->load($filename);
+		chdir('../');
+		chdir('TinyPictures');
+		$image->resizeToWidth(600);
+		$image->save($filename);
+	}
+	else
+	{
+		chdir('StreamPictures');
+		chdir('Pictures');
+		$image = new SimpleImage();
+		$image->load($filename);
+		chdir('../');
+		chdir('TinyPictures');
+		$image->resizeToHeight(600);
+		$image->save($filename);
+	}
+
+	  $url = 'http://75.101.134.112/api/uploadPhoto.php?picture=' . $pictureFilePath . '&streamID=' . $streamID . '&phone=' . $phone . '&tiny=' . $tinyPictureFilePath;
+	  $ch = curl_init($url);
+	  $response = curl_exec($ch);
+	  curl_close($ch);
 }
-else
-{
-	chdir('StreamPictures');
-	chdir('Pictures');
-	$image = new SimpleImage();
-	$image->load($filename);
-	chdir('../');
-	chdir('TinyPictures');
-	$image->resizeToHeight(600);
-	$image->save($filename);
+
+else{
+	$errorArray = array();
+
+	$errorArray['error'] = "Photo did not upload";
+
+	echo json_encode($errorArray);
 }
 
-  $url = 'http://75.101.134.112/api/uploadPhoto.php?picture=' . $pictureFilePath . '&streamID=' . $streamID . '&phone=' . $phone . '&tiny=' . $tinyPictureFilePath;
-  $ch = curl_init($url);
-  $response = curl_exec($ch);
-  curl_close($ch);
-  
 ?>
