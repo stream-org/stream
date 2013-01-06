7b919d5d650247f380d928dd2fd2517f51f72274
Rousseau Kazi <kazi.rousseau@gmail.com>
2013-01-05 22:42:39 -0800

    sendText

--- PRODUCTION/api/dependencies.php
+++ PRODUCTION/api/dependencies.php
@@ -6,5 +6,6 @@ require_once('DEPENDENCIES/format_phone_numbers.php');
 require_once('DEPENDENCIES/mixPanel.php');
 require_once('DEPENDENCIES/push.php');
 require_once('DEPENDENCIES/SimpleImage.php'); 
+require_once('DEPENDENCIES/sendText.php'); 
 
 ?>
