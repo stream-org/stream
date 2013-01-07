99721a196f33297192fa07121666ffeeb97a287f
sumanv <svenkat45@gmail.com>
2013-01-06 16:19:52 -0800

    Picture upload files working

--- PRODUCTION/api/upload_file.php
+++ PRODUCTION/api/upload_file.php
@@ -15,32 +15,27 @@ $stream_id = $_POST['stream_id'];
 if(($_FILES["file"]["size"] > 10) && ($_FILES["file"]["size"] < 20000000))
 {
 
-
+	// Grabs the picture - POST
 	$_FILES["file"]["name"] = hash('sha512', time()) . '.jpg';
 	$filename = $_FILES["file"]["name"];
 
+	// Changes to the upload directory
 	chdir('../');
 	chdir('../');
 	chdir('upload');
 
-	$result = move_uploaded_file($_FILES["file"]["tmp_name"], 'StreamPictures/Pictures/' . $filename);
+	// uploads file to EC2 Server under html/upload/StreamPictures/Pictures
+	move_uploaded_file($_FILES["file"]["tmp_name"], 'StreamPictures/Pictures/' . $filename);
 
-	echo "<br>";
-
-	echo $result;
-	echo "<br>";
-
-	echo $filename;
-
+	// Creating the filepath string for the picture and the tinyPicture
 	$filePath = 'StreamPictures/Pictures/' . $filename;
 
 	$pictureFilePath = 'http://75.101.134.112/upload/StreamPictures/Pictures/' . $filename;
 	$tinyPictureFilePath = 'http://75.101.134.112/upload/StreamPictures/TinyPictures/' . $filename;
 
-
 	list($width, $height) = getimagesize($filePath);
 
-	
+	// changes directory to the TinyPictures folder
 	chdir('StreamPictures');
 	chdir('Pictures');
 	$image = new SimpleImage();
@@ -48,6 +43,7 @@ if(($_FILES["file"]["size"] > 10) && ($_FILES["file"]["size"] < 20000000))
 	chdir('../');
 	chdir('TinyPictures');
 
+	// resizes picture to tiny picture (600x600)
 	if(intval($width) >= intval($height))
 	{
 		$image->resizeToWidth(600);
@@ -58,7 +54,7 @@ if(($_FILES["file"]["size"] > 10) && ($_FILES["file"]["size"] < 20000000))
 	}
 	$image->save($filename);
 
-
+	// calls uploadPhoto to upload file paths to database
 	$url = 'http://75.101.134.112/stream/api/uploadPhoto.php?picture_url=' . $pictureFilePath . '&stream_id=' . $stream_id . '&uploader_phone=' . $uploader_phone . '&tiny_picture_url=' . $tinyPictureFilePath;
 	$ch = curl_init($url);
 	$response = curl_exec($ch);
@@ -70,7 +66,8 @@ else
 {
 	$output = array();
 
-	$output['error'] = "Photo did not upload";
+	$output['status'] = "error";
+	$output['error_description'] = "Photo did not upload!";
 
 	echo json_encode($output);
 }
