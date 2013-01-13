3528d39c791c6eb0f7df632e4fa3ee4b748609ba
sumanv <svenkat45@gmail.com>
2013-01-13 13:54:56 -0800

    API Complete: populate_stream_profile, add_comment, get_users_in_stream

--- PRODUCTION/1.0/api/get_users_in_stream.php
+++ PRODUCTION/1.0/api/get_users_in_stream.php
@@ -6,16 +6,22 @@
 //	stream_id
 
 //output::
-//	array of
-//		name
-// 		phone
-//		how many photos they have uploaded 
+//  status
+//  stream_id
+//	Users which is an array of users that are part of the stream ordered chronoligically by join date that includes
+//		- uploader_first
+// 		- uploader_last
+//		- uploader_picturecount
+
+// example:
+// http://75.101.134.112/stream/1.0/api/get_users_in_stream.php?stream_id=6933fb99f4867cb94b1e0e32287bb12df8636de1386bebf73b54442011bd15a1775eef990f9595cfd052a2f2da8f185145f2931a0bf0fa4a947d0e9a59eb52a5
 
 include('dependencies.php');
 
 //grabbing the arguments 
 $stream_id = $_GET['stream_id'];
 $output = array();
+$Users = array();
 
 //Gets name and number of photos uploaded for every user in the particular stream
 $picture_id_result = mysql_query("SELECT * FROM UserStreams WHERE StreamID='$stream_id' ORDER BY StreamJoinDate ASC");
@@ -40,12 +46,13 @@ while($picture_id_row = mysql_fetch_array($picture_id_result))
 	}
 
 	$uploader_array = array('uploader_phone'=>$uploader_phone,'uploader_first'=>$uploader_first, 'uploader_last'=>$uploader_last, 'uploader_picturecount'=>$uploader_picturecount);
-	array_push($output,$uploader_array);
+	array_push($Users,$uploader_array);
 
 }
 
+$output['Users'] = $Users;
 $output['status'] = "ok";
-$output['$stream_id'] = $stream_id;
+$output['stream_id'] = $stream_id;
 
 
 echo json_encode($output);
--- PRODUCTION/1.0/api/get_users_who_like_picture.php
+++ PRODUCTION/1.0/api/get_users_who_like_picture.php
@@ -1,22 +1,23 @@
 <?PHP
 
-// Gets the number of users who like the picture
+// Gets the number of users who like a particular picture
 
 //input::
-//	pictureID
+//	picture_id
 
 //output::
-//  array of phone numbers and names of people who liked it 
+//  Likers which is an array of people who liked it that includes
+// 		-liker_first
+// 		-liker_last
+// 		-liker_phone
 
 include('dependencies.php');
-
 
 //grabbing the arguments 
 $picture_id = $_GET['picture_id'];
 
 $output = array();
-
-
+$Likers = array();
 
 // Gets people who like a pictures
 $liker_result = mysql_query("SELECT First, Last, PictureLikes.Phone as Phone FROM PictureLikes INNER JOIN Users ON PictureLikes.Phone = Users.Phone WHERE PictureID='$picture_id' ORDER BY Created ASC");
@@ -29,11 +30,12 @@ while ($liker_row = mysql_fetch_array($liker_result))
 
 	$liker_array = array('liker_phone'=>$liker_phone,'liker_first'=>$liker_first, 'liker_last'=>$liker_last);
 
-	array_push($output, $liker_array);
+	array_push($Likers, $liker_array);
 
 	
 }
 
+$output['Likers'] = $Likers;
 $output['status'] = "ok";
 $output['picture_id'] = $picture_id;
 
