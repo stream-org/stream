9f1302a56a89856817dae1735ffb96da501af8ff
Channing <cchanningallen@gmail.com>
2013-01-13 22:34:23 -0800

    all pages essentially done, some formatting tweaks needed; converted css file to .less

--- channing/index.js
+++ channing/index.js
@@ -4,3 +4,10 @@ $('stream_button').click(function() {
 	console.log($(this).id);		
 });
 
+$( document ).on( 'pageinit', function(){
+	pic_width = $('.ui-block-a').width();
+	pic_height = $('.ui-block-b').width();
+
+	console.log(pic_width);
+	console.log(pic_height);
+});
